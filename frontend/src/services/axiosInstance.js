aa3f6de6e3fb1fb2e97b5c4ca6d71b8866d6d9a0
pritam2615 <pritamsuthar2003@gmail.com>
2025-06-13 22:50:04 +0530

    app.js update

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, 
+  baseURL: "https://fintrack-l7ab.onrender.com", 
   withCredentials: true, 
 });
 
