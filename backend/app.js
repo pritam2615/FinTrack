356536415269e520eed1614d4bff19b33a78c066
pritam2615 <pritamsuthar2003@gmail.com>
2025-06-13 11:37:28 +0530

    updated on docker

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,6 @@ app.use("/api/budget", budgetRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is connected to port: ${PORT}`);
 });
