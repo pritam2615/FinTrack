30b69d9c29a43f420be799a30d3de0e9c39f0308
pritam2615 <pritamsuthar2003@gmail.com>
2025-06-13 22:32:37 +0530

    app.js update

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://fintrack-1-3757.onrender.com", 
+  origin: "https://fintrack-1-nds5.onrender.com", 
   // origin: "http://localhost:5173",
   credentials: true, 
 }));
